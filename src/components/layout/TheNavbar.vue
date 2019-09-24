62a749bf958b4bc9f858f4e4cf01a93660eef270
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2019-09-24 14:40:40 -0300

    #44 Add project fonts and more templates

---
+++ src/components/layout/TheNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="nav">
+		<div class="header">	
+			<router-link to="/">Home</router-link> |
+			<router-link to="/signin">Sign in</router-link> |
+			<router-link to="/signup">Sign up</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+	width: 100%;
+	height: 40px;
+	background-color: darkslategray;
+}
+</style>
+
